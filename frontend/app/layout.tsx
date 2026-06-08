04a8c799aafacd3fda85edfd52dfc2f2af6fef99
ryu <jryu0717@naver.com>
2026-06-08 16:13:57 +0900

    fix: 구글 서치 콘솔 인증 메타 태그 코드 수정

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'FaWW(파우) | AI 체형분석 기반 임직원 근골격계 피지컬케어 EAP',
   description: '거북목, 허리 통증 솔루션! 대기업·스타트업이 선택한 찾아가는 사내 임직원 건강 복지 프로그램, 데이터 기반 피지컬케어 No.1 파우.',
   verification: {
-    google: 'f4ad7ff4480181ab',
+    google: '1CKTUnV7zTqw0YWxzXDFNN0TBw_ZG6CVQo5xci6DOK8',
   },
 };
 
